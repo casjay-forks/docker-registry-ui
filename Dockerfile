580eece8d98116c21c6d9bd480e8b7bafc9aba3f
casjay <git-admin@casjaysdev.com>
2022-10-07 19:58:43 -0400

    🗃️ Committing everything that changed 🗃️

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,56 @@
-FROM jc21/node:latest
+FROM casjaysdevdocker/alpine:latest as build
 
-MAINTAINER Jamie Curnow <jc@jc21.com>
-LABEL maintainer="Jamie Curnow <jc@jc21.com>"
+ARG LICENSE="WTFPL" \
+  IMAGE_NAME="docker-registry-ui" \
+  TIMEZONE="America/New_York" \
+  LICENSE="MIT" \
+  NODE_ENV=production \
+  PORT="80"
 
-RUN apt-get update \
-    && apt-get install -y curl \
-    && apt-get clean
+ENV SHELL=/bin/bash \
+  TERM=xterm-256color \
+  HOSTNAME=${HOSTNAME:-casjaysdev-$IMAGE_NAME} \
+  TZ=$TIMEZONE
 
-ENV NODE_ENV=production
+RUN apk update -U --no-cache
 
-ADD dist                /app/dist
-ADD node_modules        /app/node_modules
-ADD LICENCE             /app/LICENCE
-ADD package.json        /app/package.json
-ADD src/backend         /app/src/backend
+COPY ./bin/. /usr/local/bin/
+COPY ./config/. /config/
+COPY ./data/. /data/
 
-WORKDIR /app
+RUN mkdir -p /bin/ /config/ /data/ && \
+  rm -Rf /bin/.gitkeep /config/.gitkeep /data/.gitkeep
 
-CMD node --max_old_space_size=250 --abort_on_uncaught_exception src/backend/index.js
+FROM scratch
+ARG BUILD_DATE="2022-10-07 19:46" \
+  BUILD_VERSION="latest"
 
-HEALTHCHECK --interval=15s --timeout=3s CMD curl -f http://localhost/ || exit 1
+LABEL org.label-schema.name="docker-registry-ui" \
+  org.label-schema.description="Containerized version of docker-registry-ui" \
+  org.label-schema.url="https://hub.docker.com/r/casjaysdevdocker/docker-registry-ui" \
+  org.label-schema.vcs-url="https://github.com/casjaysdevdocker/docker-registry-ui" \
+  org.label-schema.build-date="#BUILD_VERSION" \
+  org.label-schema.version="" \
+  org.label-schema.vcs-ref="" \
+  org.label-schema.license="" \
+  org.label-schema.vcs-type="Git" \
+  org.label-schema.schema-version="" \
+  org.label-schema.vendor="CasjaysDev" \
+  maintainer="CasjaysDev <docker-admin@casjaysdev.com>"
 
+ENV SHELL="/bin/bash" \
+  TERM="xterm-256color" \
+  HOSTNAME="casjaysdev-docker-registry-ui" \
+  TZ="${TZ:-America/New_York}"
+
+WORKDIR /root
+
+VOLUME ["/root","/config","/data"]
+
+EXPOSE $PORT
+
+COPY --from=build /. /
+
+ENTRYPOINT [ "tini", "--" ]
+CMD [ "/usr/local/bin/entrypoint-docker-registry-ui.sh" ]
+HEALTHCHECK --start-period=1m --interval=2m --timeout=3s CMD [ "/usr/local/bin/entrypoint-docker-registry-ui.sh", "healthcheck" ]
---
+++ bin/entrypoint-docker-registry-ui.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202210071946-git
+# @@Author           :  Jason Hempstead
+# @@Contact          :  jason@casjaysdev.com
+# @@License          :  WTFPL
+# @@ReadME           :  entrypoint-docker-registry-ui.sh --help
+# @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @@Created          :  Friday, Oct 07, 2022 19:46 EDT
+# @@File             :  entrypoint-docker-registry-ui.sh
+# @@Description      :
+# @@Changelog        :  New script
+# @@TODO             :  Better documentation
+# @@Other            :
+# @@Resource         :
+# @@Terminal App     :  no
+# @@sudo/root        :  no
+# @@Template         :  other/docker-entrypoint
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="$(basename "$0" 2>/dev/null)"
+VERSION="202210071946-git"
+HOME="${USER_HOME:-$HOME}"
+USER="${SUDO_USER:-$USER}"
+RUN_USER="${SUDO_USER:-$USER}"
+SCRIPT_SRC_DIR="${BASH_SOURCE%/*}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set bash options
+[ "$1" == "--debug" ] && set -xo pipefail && export SCRIPT_OPTS="--debug" && export _DEBUG="on"
+[ "$1" == "--raw" ] && export SHOW_RAW="true"
+set -o pipefail
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set functions
+__version() { echo -e ${GREEN:-}"$VERSION"${NC:-}; }
+__find() { ls -A "$*" 2>/dev/null; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# colorization
+[ -n "$SHOW_RAW" ] || printf_color() { echo -e '\t\t'${2:-}"${1:-}${NC}"; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__exec_bash() {
+  local cmd="${*:-/bin/bash}"
+  local exitCode=0
+  echo "Executing command: $cmd"
+  $cmd || exitCode=10
+  return ${exitCode:-$?}
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Define default variables
+TZ="${TZ:-America/New_York}"
+HOSTNAME="${HOSTNAME:-casjaysdev-bin}"
+BIN_DIR="${BIN_DIR:-/usr/local/bin}"
+DATA_DIR="${DATA_DIR:-$(__find /data/ 2>/dev/null | grep '^' || false)}"
+CONFIG_DIR="${CONFIG_DIR:-$(__find /config/ 2>/dev/null | grep '^' || false)}"
+CONFIG_COPY="${CONFIG_COPY:-false}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional variables
+export NODE_ENV="${NODE_ENV:-production}"
+export REGISTRY_SSL="${REGISTRY_SSL:-false}"
+export REGISTRY_HOST="${REGISTRY_HOST:-http://172.17.0.1:5000}"
+export REGISTRY_DOMAIN="${REGISTRY_DOMAIN:-$REGISTRY_HOST}"
+export REGISTRY_STORAGE_DELETE_ENABLED="${REGISTRY_STORAGE_DELETE_ENABLED:-false}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Export variables
+export TZ HOSTNAME
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# import variables from file
+[ -f "/root/env.sh" ] && . "/root/env.sh"
+[ -f "/config/.env.sh" ] && . "/config/.env.sh"
+[ -f "/root/env.sh" ] && [ ! -f "/config/.env.sh" ] && cp -Rf "/root/env.sh" "/config/.env.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set timezone
+[ -n "${TZ}" ] && echo "${TZ}" >/etc/timezone
+[ -f "/usr/share/zoneinfo/${TZ}" ] && ln -sf "/usr/share/zoneinfo/${TZ}" "/etc/localtime"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set hostname
+if [ -n "${HOSTNAME}" ]; then
+  echo "${HOSTNAME}" >/etc/hostname
+  echo "127.0.0.1 ${HOSTNAME} localhost ${HOSTNAME}.local" >/etc/hosts
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Delete any gitkeep files
+[ -n "${CONFIG_DIR}" ] && { [ -d "${CONFIG_DIR}" ] && rm -Rf "${CONFIG_DIR}/.gitkeep" || mkdir -p "/config/"; }
+[ -n "${DATA_DIR}" ] && { [ -d "${DATA_DIR}" ] && rm -Rf "${DATA_DIR}/.gitkeep" || mkdir -p "/data/"; }
+[ -n "${BIN_DIR}" ] && { [ -d "${BIN_DIR}" ] && rm -Rf "${BIN_DIR}/.gitkeep" || mkdir -p "/bin/"; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy config files to /etc
+if [ -n "${CONFIG_DIR}" ] && [ "${CONFIG_COPY}" = "true" ]; then
+  for config in ${CONFIG_DIR}; do
+    if [ -d "/config/$config" ]; then
+      [ -d "/etc/$config" ] || mkdir -p "/etc/$config"
+      cp -Rf "/config/$config/." "/etc/$config/"
+    elif [ -f "/config/$config" ]; then
+      cp -Rf "/config/$config" "/etc/$config"
+    fi
+  done
+fi
+[ -f "/etc/.env.sh" ] && rm -Rf "/etc/.env.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Additional commands
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+case "$1" in
+--help) # Help message
+  echo 'Docker container for '$APPNAME''
+  echo "Usage: $APPNAME [healthcheck, bash, command]"
+  echo "Failed command will have exit code 10"
+  echo
+  exitCode=$?
+  ;;
+
+healthcheck) # Docker healthcheck
+  curl -q -Lssf "http://localhost"
+  exitCode=$?
+  ;;
+
+*/bin/sh | */bin/bash | bash | shell | sh) # Launch shell
+  shift 1
+  __exec_bash "${@:-/bin/bash}"
+  exitCode=$?
+  ;;
+
+*) # Execute primary command
+  if [ $# -eq 0 ]; then
+    node --max_old_space_size=250 --abort_on_uncaught_exception src/backend/index.js
+  else
+    __exec_bash "/bin/bash"
+  fi
+  exitCode=$?
+  ;;
+esac
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end of entrypoint
+exit ${exitCode:-$?}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
